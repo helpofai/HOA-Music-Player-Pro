602a0d4c1706a37794e5ca572ea755158bb03fdc
helpofai <contact@helpofai.com>
2026-01-31 17:18:03 +0530

    Feature: Integrate GitHub Issue reporting into crash activity

--- app/src/main/java/com/helpofai/hoa/musicplayer/activities/ErrorActivity.kt
+++ app/src/main/java/com/helpofai/hoa/musicplayer/activities/ErrorActivity.kt
@@ -1,21 +1,22 @@
 package com.helpofai.hoa.musicplayer.activities
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import cat.ereza.customactivityoncrash.CustomActivityOnCrash
 import com.helpofai.hoa.musicplayer.R
-import com.helpofai.hoa.musicplayer.util.FileUtils.createFile
-import com.helpofai.hoa.musicplayer.util.Share.shareFile
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import java.text.DateFormat
-import java.text.SimpleDateFormat
+import java.net.URLEncoder
 import java.util.*
 
 class ErrorActivity : AppCompatActivity() {
-    private val dayFormat: DateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-    private val reportPrefix = "bug_report-"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(cat.ereza.customactivityoncrash.R.layout.customactivityoncrash_default_error_activity)
@@ -39,32 +40,24 @@ class ErrorActivity : AppCompatActivity() {
             findViewById<Button>(cat.ereza.customactivityoncrash.R.id.customactivityoncrash_error_activity_more_info_button)
 
         moreInfoButton.setOnClickListener { //We retrieve all the error data and show it
+            val errorDetails = CustomActivityOnCrash.getAllErrorDetailsFromIntent(
+                this@ErrorActivity,
+                intent
+            )
             MaterialAlertDialogBuilder(this@ErrorActivity)
                 .setTitle(cat.ereza.customactivityoncrash.R.string.customactivityoncrash_error_activity_error_details_title)
-                .setMessage(
-                    CustomActivityOnCrash.getAllErrorDetailsFromIntent(
-                        this@ErrorActivity,
-                        intent
-                    )
-                )
+                .setMessage(errorDetails)
                 .setPositiveButton(
                     cat.ereza.customactivityoncrash.R.string.customactivityoncrash_error_activity_error_details_close,
                     null
                 )
+                .setNegativeButton("Copy") { _, _ ->
+                    copyToClipboard(errorDetails)
+                }
                 .setNeutralButton(
                     R.string.customactivityoncrash_error_activity_error_details_share
                 ) { _, _ ->
-
-                    val bugReport = createFile(
-                        context = this,
-                        "Bug Report",
-                        "$reportPrefix${dayFormat.format(Date())}",
-                        CustomActivityOnCrash.getAllErrorDetailsFromIntent(
-                            this@ErrorActivity,
-                            intent
-                        ), ".txt"
-                    )
-                    shareFile(this, bugReport, "text/*")
+                    reportOnGithub(errorDetails)
                 }
                 .show()
         }
@@ -75,6 +68,24 @@ class ErrorActivity : AppCompatActivity() {
             errorImageView.setImageResource(
                 errorActivityDrawableId
             )
+        }
+    }
+
+    private fun copyToClipboard(text: String) {
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("Crash Report", text)
+        clipboard.setPrimaryClip(clip)
+        Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun reportOnGithub(errorDetails: String) {
+        try {
+            val body = URLEncoder.encode("### Crash Report\n\n```\n$errorDetails\n```", "UTF-8")
+            val safeBody = if (body.length > 2000) body.substring(0, 2000) + "..." else body
+            val url = "https://github.com/helpofai/HOA-Music-Player-Pro/issues/new?labels=bug&title=Crash%20Report&body=$safeBody"
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+        } catch (e: Exception) {
+            Toast.makeText(this, "Could not open browser", Toast.LENGTH_SHORT).show()
         }
     }
 }
